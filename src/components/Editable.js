7024f47e42488d37c4f73ea2d64197cb5341f7ee
Parham Abolghasemi <parhamab17@gmail.com>
2022-08-17 10:13:11 +0430

    editable component updated

--- src/components/Editable.js
+++ src/components/Editable.js
@@ -1,9 +1,58 @@
+// bootstrap
+import Form from "react-bootstrap/Form";
+
 const Editable = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <>
+      <tr>
+        {/* <td>{item.priority}</td> */}
+        <td>
+          <Form.Control
+            className="m-1"
+            type="text"
+            name="fullName"
+            required
+            placeholder="Enter the name..."
+            //   onChange={addFormHandle}
+            //   ref={nameVal}
+          />
+        </td>
+        <td>
+          <Form.Control
+            className="m-1"
+            type="text"
+            name="email"
+            required
+            placeholder="Enter the email..."
+            //   onChange={addFormHandle}
+            //   ref={nameVal}
+          />
+        </td>
+        <td>
+          <Form.Control
+            className="m-1"
+            type="text"
+            name="phoneNo"
+            required
+            placeholder="Enter the PhoneNo..."
+            //   onChange={addFormHandle}
+            //   ref={nameVal}
+          />
+        </td>
+        <td>
+          <Form.Control
+            className="m-1"
+            type="text"
+            name="address"
+            required
+            placeholder="Enter the Address..."
+            //   onChange={addFormHandle}
+            //   ref={nameVal}
+          />
+        </td>
+      </tr>
+    </>
+  );
 };
 
 export default Editable;
