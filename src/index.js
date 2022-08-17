d2856accdd69bf8ef5402eccdd8a68db85a87048
Parham Abolghasemi <parhamab17@gmail.com>
2022-08-17 08:12:35 +0430

    needed packages installed, folder structure updated, initial styles, fonts added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+// styles
+import "./assets/styles/index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
